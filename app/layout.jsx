31ef9ac763beb117f720fe7cffffbb5c33258436
penn <penn201500@gmail.com>
2024-08-31 15:27:50 +0800

    add metadata and assets: favicon,images

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css"
 
+export const metadata = {
+    title: "Property Management",
+    keywords: "rental,property,real estate",
+    description: "Property management made easy",
+}
+
 const MainLayout = ({ children }) => {
     return (
         <html>
