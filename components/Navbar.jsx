aebedd725c620205ffaffb9e8ff0982d1c72a803
penn <penn201500@gmail.com>
2024-09-01 20:28:03 +0800

    use usePathname to set background color in Navbar for mobile dropdown menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -180,17 +180,17 @@ const Navbar = () => {
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         <Link
                             href="/"
-                            className="bg-black text-white block rounded-md px-3 py-2 text-base font-medium">
+                            className={`${pathname === "/" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
                             Home
                         </Link>
                         <Link
                             href="/properties"
-                            className="text-white block rounded-md px-3 py-2 text-base font-medium">
+                            className={`${pathname === "/properties" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
                             Properties
                         </Link>
                         <Link
                             href="/properties/add"
-                            className="text-white block rounded-md px-3 py-2 text-base font-medium">
+                            className={`${pathname === "/properties/add" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
                             Add Property
                         </Link>
                         <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
