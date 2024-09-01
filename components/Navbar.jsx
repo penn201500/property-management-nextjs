f6a3b668a8f8a0b0ad56ffc8a7bb5acaad611055
penn <penn201500@gmail.com>
2024-09-01 19:39:48 +0800

    add state to hide and display profile dropdown menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ import profileDefault from "@/assets/images/profile.png"
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+    const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
 
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
@@ -116,6 +117,7 @@ const Navbar = () => {
                         <div className="relative ml-3">
                             <div>
                                 <button
+                                    onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}
                                     type="button"
                                     className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                     id="user-menu-button"
@@ -132,37 +134,39 @@ const Navbar = () => {
                             </div>
 
                             {/* <!-- Profile dropdown --> */}
-                            <div
-                                id="user-menu"
-                                className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu"
-                                aria-orientation="vertical"
-                                aria-labelledby="user-menu-button"
-                                tabIndex="-1">
-                                <Link
-                                    href="/profile"
-                                    className="block px-4 py-2 text-sm text-gray-700"
-                                    role="menuitem"
-                                    tabIndex="-1"
-                                    id="user-menu-item-0">
-                                    Your Profile
-                                </Link>
-                                <Link
-                                    href="/properties/save"
-                                    className="block px-4 py-2 text-sm text-gray-700"
-                                    role="menuitem"
-                                    tabIndex="-1"
-                                    id="user-menu-item-2">
-                                    Saved Properties
-                                </Link>
-                                <button
-                                    className="block px-4 py-2 text-sm text-gray-700"
-                                    role="menuitem"
-                                    tabIndex="-1"
-                                    id="user-menu-item-2">
-                                    Sign Out
-                                </button>
-                            </div>
+                            {isProfileMenuOpen && (
+                                <div
+                                    id="user-menu"
+                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu"
+                                    aria-orientation="vertical"
+                                    aria-labelledby="user-menu-button"
+                                    tabIndex="-1">
+                                    <Link
+                                        href="/profile"
+                                        className="block px-4 py-2 text-sm text-gray-700"
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        id="user-menu-item-0">
+                                        Your Profile
+                                    </Link>
+                                    <Link
+                                        href="/properties/save"
+                                        className="block px-4 py-2 text-sm text-gray-700"
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        id="user-menu-item-2">
+                                        Saved Properties
+                                    </Link>
+                                    <button
+                                        className="block px-4 py-2 text-sm text-gray-700"
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        id="user-menu-item-2">
+                                        Sign Out
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
