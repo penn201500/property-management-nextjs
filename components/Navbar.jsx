1909e160a80c68d255f9e82257f59ce9521d0887
penn <penn201500@gmail.com>
2024-09-01 20:35:20 +0800

    hide or display Add Property menu according to isLoggedIn state

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -195,15 +195,19 @@ const Navbar = () => {
                             className={`${pathname === "/properties" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
                             Properties
                         </Link>
-                        <Link
-                            href="/properties/add"
-                            className={`${pathname === "/properties/add" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
-                            Add Property
-                        </Link>
-                        <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                            <i className="fa-brands fa-google mr-2"></i>
-                            <span>Login or Register</span>
-                        </button>
+                        {isLoggedIn && (
+                            <Link
+                                href="/properties/add"
+                                className={`${pathname === "/properties/add" ? "bg-black" : ""} text-white block rounded-md px-3 py-2 text-base font-medium`}>
+                                Add Property
+                            </Link>
+                        )}
+                        {!isLoggedIn && (
+                            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
+                                <i className="fa-brands fa-google mr-2"></i>
+                                <span>Login or Register</span>
+                            </button>
+                        )}
                     </div>
                 </div>
             )}
