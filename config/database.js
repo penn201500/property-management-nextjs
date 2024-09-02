6e6dc30e2ceaf928b3b108f7e20a36cd627e0927
penn <penn201500@gmail.com>
2024-09-02 18:22:21 +0800

    connect to MongoDB

---
+++ config/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+let connected = false
+
+async function connectDB() {
+    mongoose.set("stricQuery", true)
+
+    // If the db is connected, don't connect again
+    if (connected) {
+        console.log("=> using existing database connection")
+        return
+    }
+    // Connect to MongoDB
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        connected = true
+    } catch (error) {
+        console.log("Error connecting to database: ", error)
+    }
+}
+
+export default connectDB
