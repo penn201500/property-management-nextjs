96b65b4e1267871dcf23a5b3ee2bd44ae26462d9
penn <penn201500@gmail.com>
2024-09-02 11:52:34 +0800

    1. add properties.json as test data; 2. display property.name on property page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,21 @@
+import properties from "@/properties.json"
+
 const PropertiesPage = () => {
-    return <div>Propertiesi Page</div>
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container mx-auto px-4 py-6">
+                {properties.length === 0 ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map(property => {
+                            return <div key={property._id}>{property.name}</div>
+                        })}
+                    </div>
+                )}
+            </div>
+        </section>
+    )
 }
 
 export default PropertiesPage
