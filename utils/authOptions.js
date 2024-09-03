2ab890b70a356df404b501242717ad0b3157cf2e
penn <penn201500@gmail.com>
2024-09-03 22:20:45 +0800

    add authprovider to use google oauth provider to verify user login

---
+++ utils/authOptions.js
@@ -0,0 +1,21 @@
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    access_type: "offline",
+                    prompt: "consent",
+                    response_type: "code",
+                },
+            },
+        }),
+    ],
+    callbacks: {
+        async singIn({ profile }) {},
+        async session({ session }) {},
+    },
+}
