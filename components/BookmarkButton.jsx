ac13dd9e34632cea8d755867ad87720afcf59477
penn <penn201500@gmail.com>
2024-09-07 15:44:23 +0800

    fix: Warning: Invalid DOM property `class`. Did you mean `className`

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,8 +1,13 @@
+"use client"
+
 import { FaBookmark } from "react-icons/fa"
+import { useState } from "react"
+import bookmarkProperty from "@/app/actions/bookmarkProperty"
+import { toast } from "react-toastify"
 
 const BookmarkButton = ({ property }) => {
     return (
-        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
             <FaBookmark className="mr-2" />
             Bookmark Property
         </button>
