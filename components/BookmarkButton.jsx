d33eca9ba11aba16f0747b216a8dabf67b21ac8f
penn <penn201500@gmail.com>
2024-09-07 16:36:55 +0800

    add bookmarks to mongodb

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -4,10 +4,33 @@ import { FaBookmark } from "react-icons/fa"
 import { useState } from "react"
 import bookmarkProperty from "@/app/actions/bookmarkProperty"
 import { toast } from "react-toastify"
+import { useSession } from "next-auth/react"
 
 const BookmarkButton = ({ property }) => {
+    const { data: session } = useSession()
+    const userId = session?.user?.id
+
+    // const [bookmarked, setBookmarked] = useState(property.bookmarkedBy.includes(userId))
+
+    const handleClick = async () => {
+        if (!userId) {
+            toast.error("You must be logged in to bookmark a property")
+            return
+        }
+
+        try {
+            const { message, isBookmarked } = await bookmarkProperty(property._id)
+            toast.success(message)
+            // setBookmarked(isBookmarked)
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     return (
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+        <button
+            onClick={handleClick}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
             <FaBookmark className="mr-2" />
             Bookmark Property
         </button>
