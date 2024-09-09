fc1cd3a0d9fbbd7dee7b09b49f7ad3dbbdc2cd18
penn <penn201500@gmail.com>
2024-09-09 21:27:39 +0800

    message submission

---
+++ app/actions/addMessage.js
@@ -0,0 +1,37 @@
+"use server"
+import connectDB from "@/config/database"
+import Message from "@/models/Message"
+import { getSessionUser } from "@/utils/getSessionUser"
+
+async function addMessage(previousState, formData) {
+    await connectDB()
+    const sessionUser = await getSessionUser()
+
+    if (!sessionUser || !sessionUser.user) {
+        throw new Error("You must be logged in to add a property")
+    }
+
+    const { userId } = sessionUser
+
+    const recipient = formData.get("recipient")
+
+    if (userId === recipient) {
+        return { error: "You cannot send a message to yourself" }
+    }
+
+    const newMessage = new Message({
+        sender: userId,
+        recipient,
+        property: formData.get("property"),
+        name: formData.get("name"),
+        email: formData.get("email"),
+        phone: formData.get("phone"),
+        body: formData.get("body"),
+    })
+
+    await newMessage.save()
+
+    return { submitted: true }
+}
+
+export default addMessage
