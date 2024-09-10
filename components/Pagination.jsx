470fb3f097dfc55cd6f616fa9ad872c07a61b07e
penn <penn201500@gmail.com>
2024-09-11 06:42:33 +0800

    update link and page number styles

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -6,19 +6,18 @@ const Pagination = ({ page, pageSize, totalItems }) => {
         <section className="container mx-auto flex justify-center items-center my-8">
             {page > 1 ? (
                 <Link
-                    className="mr-2 px-2 py-1 border border-gray-300 items-center my-8"
+                    className="mr-2 px-2 py-1 border border-gray-300 items-center my-8 bg-blue-500 text-white rounded-md"
                     href={`/properties?page=${page - 1}`}>
                     Previous
                 </Link>
             ) : null}
 
             <span className="mx-2">
-                {" "}
-                Page {page} of {totalPages}
+                Page <span className="text-blue-700">{page}</span> of <span className="text-blue-700">{totalPages}</span>
             </span>
             {page < totalPages ? (
                 <Link
-                    className="mr-2 px-2 py-1 border border-gray-300 items-center my-8"
+                    className="mr-2 px-2 py-1 border border-gray-300 items-center my-8 bg-blue-500 text-white rounded-md"
                     href={`/properties?page=${page + 1}`}>
                     Next
                 </Link>
