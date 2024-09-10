b0525c358578e279404e6dfc2736f07b8919aa5c
penn <penn201500@gmail.com>
2024-09-10 16:25:58 +0800

    create global context for message count

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,9 @@ import "@/assets/styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import AuthProvider from "@/components/AuthProvider"
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import { GlobalProvider } from "@/context/GlobalContext"
 
 export const metadata = {
     title: "Property Management",
@@ -14,14 +15,16 @@ export const metadata = {
 const MainLayout = ({ children }) => {
     return (
         <AuthProvider>
-            <html>
-                <body>
-                    <Navbar />
-                    <main>{children}</main>
-                    <Footer />
-                    <ToastContainer />
-                </body>
-            </html>
+            <GlobalProvider>
+                <html>
+                    <body>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Footer />
+                        <ToastContainer />
+                    </body>
+                </html>
+            </GlobalProvider>
         </AuthProvider>
     )
 }
