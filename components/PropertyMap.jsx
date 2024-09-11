7cd534d6a1a50df28fd74488bf543dac01aced09
penn <penn201500@gmail.com>
2024-09-11 13:07:39 +0800

    mapbox is buggy, use google map api

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,26 +1,23 @@
 "use client"
 import { useState, useEffect } from "react"
 import { setDefaults, fromAddress } from "react-geocode"
-import Map, { Marker } from "react-map-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
-import Image from "next/image"
-import pin from "@/assets/images/pin.svg"
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"
 import Spinner from "@/components/Spinner"
+import pin from "@/assets/images/pin.svg"
+import Image from "next/image"
 
 const PropertyMap = ({ property }) => {
     const [lat, setLat] = useState(null)
     const [lng, setLng] = useState(null)
-    const [viewport, setViewport] = useState({
-        latitude: 0,
-        longitude: 0,
-        zoom: 12,
-        width: "100%",
-        height: "500px",
-    })
-
     const [loading, setLoading] = useState(true)
     const [geocodeError, setGeocodeError] = useState(false)
 
+    // Load the Google Maps script
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+    })
+
+    // Set Google Geocode defaults
     setDefaults({
         key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
         language: "en",
@@ -40,12 +37,6 @@ const PropertyMap = ({ property }) => {
                 const { lat, lng } = res.results[0].geometry.location
                 setLat(lat)
                 setLng(lng)
-                setViewport({
-                    ...viewport,
-                    latitude: lat,
-                    longitude: lng,
-                })
-
             } catch (error) {
                 console.log("There was an error", error)
                 setGeocodeError(true)
@@ -56,33 +47,22 @@ const PropertyMap = ({ property }) => {
         fetchCoords()
     }, [])
 
-    if (loading) {
+    if (loading || !isLoaded) {
         return <Spinner loading={loading} />
     }
+
     if (geocodeError) {
         return <div>No location data found</div>
     }
 
     return (
-        !loading && (
-            <Map
-                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-                mapLib={import("mapbox-gl")}
-                initialViewState={{ longitude: lng, latitude: lat, zoom: 15 }}
-                style={{ width: "100%", height: 500 }}
-                mapStyle="mapbox://styles/mapbox/streets-v9">
-                <Marker
-                    latitude={lat}
-                    longitude={lng}
-                    anchor="bottom">
-                    <Image
-                        src={pin}
-                        alt="location"
-                        width={40}
-                        height={40}
-                    />
-                </Marker>
-            </Map>
+        isLoaded && (
+            <GoogleMap
+                center={{ lat: lat, lng: lng }}
+                zoom={15}
+                mapContainerStyle={{ width: "100%", height: "500px" }}>
+                <Marker position={{ lat: lat, lng: lng }} />
+            </GoogleMap>
         )
     )
 }
